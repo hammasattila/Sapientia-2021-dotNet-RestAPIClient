3249b9efbaa0c6af2aee5799d3cacb3c47e6356d
hammasattila <hammasattila@outlook.com>
2021-03-31 12:33:05 +0300

    Lecture 7. Create tyre. Update tyre. Delete tyre.

--- src/components/Tyres.js
+++ src/components/Tyres.js
@@ -6,13 +6,61 @@ export class Tyres extends React.Component {
         super(props)
         this.state = {
             selectedTyre: null,
-            tyresList: []
+            tyresList: [],
+            formIsVisible: false,
+            id: null,
+            name: "",
+            wifth: 100,
+            height: 10,
+            inch: 10,
+            price: 1
         }
+    }
+
+    handleShowCreateForm = () => {
+        const { formIsVisible } = this.state;
+        this.setState({ formIsVisible: !formIsVisible });
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const { id, name, width, height, inch, price } = this.state;
+        const tyre = {
+            name: name,
+            width: parseInt(width),
+            height: parseInt(height),
+            inch: parseInt(inch),
+            price: parseInt(price)
+        };
+
+        console.debug(id);
+        if (id) {
+            axios.put(`https://localhost:5001/items/${id}`, tyre)
+                .then(response => {
+                    console.debug(response.data);
+                    this.getTyresList();
+                }).catch(error => {
+                    console.error('API error while updateing.', error);
+                })
+        } else {
+            axios.post('', tyre)
+                .then(response => {
+                    console.debug(response.data);
+                    this.getTyresList();
+                })
+                .catch(error => {
+                    console.error("API error while creating tyre.", error);
+                });
+        }
+
     }
 
     handleSelectTyre = event => {
         const tyreId = event.target.value;
-        console.log(tyreId)
         if (this.state.selectedTyre?.id === tyreId) {
             this.setState({ selectedTyre: null });
         } else {
@@ -23,6 +71,33 @@ export class Tyres extends React.Component {
                     console.error("API error", error);
                 });
         }
+    }
+
+    handleEditTyre = event => {
+        const { tyresList, formIsVisible } = this.state;
+        const tyreId = event.target.value;
+        const selectedTyre = tyresList.find(tyre => tyre.id === tyreId)
+
+        this.setState({
+            formIsVisible: true,
+            id: tyreId,
+            name: selectedTyre.name,
+            width: selectedTyre.width,
+            height: selectedTyre.height,
+            inch: selectedTyre.inch,
+            price: selectedTyre.price,
+        })
+    }
+
+    handleDeleteTyre = event => {
+        const tyreId = event.target.value;
+        axios.delete(`https://localhost:5001/items/${tyreId}`)
+            .then(response => {
+                console.debug(response.data);
+                this.getTyresList();
+            }).catch(error => {
+                console.error("API error", error);
+            });
     }
 
     getTyresList() {
@@ -48,7 +123,7 @@ export class Tyres extends React.Component {
     }
 
     render() {
-        const { selectedTyre, tyresList } = this.state
+        const { selectedTyre, tyresList, formIsVisible, name, width, height, inch, price } = this.state
         return (
             <div className="container-wrapper">
                 <div className="conatiner">
@@ -57,7 +132,7 @@ export class Tyres extends React.Component {
                         <h3>Tyres store - powered by Topmotive</h3>
                     </header>
                     <main>
-                        <section className="tires">
+                        <section>
                             <h4 className="tires-title">
                                 <span className="tires-icon"></span>
                                 Tyres
@@ -82,8 +157,11 @@ export class Tyres extends React.Component {
                                             <div>{tire.price}</div>
                                             <div>{tire.createdDate}</div>
                                             <button className="btn arrow" value={tire.id} onClick={this.handleSelectTyre}>Select</button>
+                                            <button className="btn arrow" value={tire.id} onClick={this.handleEditTyre}>Edit</button>
+                                            <button className="btn arrow" value={tire.id} onClick={this.handleDeleteTyre}>Delete</button>
                                         </div>)
                                 }
+                                <button className="btn arrow" onClick={this.handleShowCreateForm}>Create</button>
                             </div>
                         </section>
                     </main>
@@ -112,6 +190,26 @@ export class Tyres extends React.Component {
                             <div>{selectedTyre.createdDate}</div>
                         </div>
                     </div>
+                </section>}
+                {formIsVisible && <section className="tires">
+                    <form onSubmit={this.handleSubmit} action="none">
+                        <label>Tyre name:
+                            <input type="text" name="name" value={name} onChange={this.handleChange} />
+                        </label>
+                        <label>Tyre width:
+                            <input type="number" name="width" value={width} min="100" max="400" onChange={this.handleChange} />
+                        </label>
+                        <label>Tyre height:
+                            <input type="number" name="height" value={height} min="10" max="100" onChange={this.handleChange} />
+                        </label>
+                        <label>Tyre inch:
+                            <input type="number" name="inch" value={inch} min="10" max="100" onChange={this.handleChange} />
+                        </label>
+                        <label>Tyre price:
+                            <input type="number" name="price" value={price} min="1" max="1000" onChange={this.handleChange} />
+                        </label>
+                        <button type="submit">Submit</button>
+                    </form>
                 </section>}
             </div>
         )
